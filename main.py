9443b04dbf96e91e787f7c89b96c82f80f4b9285
amaladrar <adraramal@estin.dz>
2026-02-06 12:11:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-from github_api import get_user_repos
-from analyzer import analyze_all
-from html_generator import generate_portfolio
+import yaml
+import github_api
+import analyzer
+import html_generator
 
-username = input("GitHub username: ")
+# Charger config.yaml
+with open("config.yaml", "r", encoding="utf-8") as f:
+    config = yaml.safe_load(f)
 
-repos = get_user_repos(username)
-analysis = analyze_all(username, repos)
+username = config["github"]["username"]
+token = config["github"].get("token")
 
-generate_portfolio(username, analysis)
+client = github_api.GitHubClient(token)
+repos = client.get_repos(username)
 
-print(" Portfolio HTML généré : output/portfolio.html")
+projects = []
+
+for repo in repos:
+    if config["filters"]["exclude_forks"] and repo["fork"]:
+        continue
+
+    commits = client.get_commits(
+        username=username,
+        repo_name=repo["name"],
+        limit=100  # max 100 commits
+    )
+
+    project = analyzer.analyze_repo(repo, commits)
+    projects.append(project)
+
+html_generator.generate_html(projects, config)
+
+print("✅ Portfolio généré avec succès")
