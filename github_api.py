405723600c1ce795fe50859a851bd43afbefd775
amaladrar <adraramal@estin.dz>
2026-02-06 12:09:41 +0100

    Update github_api.py

--- github_api.py
+++ github_api.py
@@ -1,32 +1,25 @@
 import requests
-from config import GITHUB_TOKEN, BASE_URL
-
-headers = {
-    "Authorization": f"token {GITHUB_TOKEN}"
-}
-
-def get_user_repos(username):
-    url = f"{BASE_URL}/users/{username}/repos"
-
-    r = requests.get(url, headers=headers)
-
-    if r.status_code != 200:
-        print("Erreur API:", r.status_code)
-        print(r.text)
-        return []
-
-    return r.json()
-def get_repo_commits(username, repo):
-    url = f"{BASE_URL}/repos/{username}/{repo}/commits"
-    r = requests.get(url, headers=headers)
-    if r.status_code != 200:
-        return []
-    return r.json()
 
 
-def get_languages(username, repo):
-    url = f"{BASE_URL}/repos/{username}/{repo}/languages"
-    r = requests.get(url, headers=headers)
-    if r.status_code != 200:
-        return {}
-    return r.json()
+class GitHubClient:
+    BASE_URL = "https://api.github.com"
+
+    def __init__(self, token=None):
+        self.headers = {
+            "Accept": "application/vnd.github+json"
+        }
+        if token:
+            self.headers["Authorization"] = f"Bearer {token}"
+
+    def get_repos(self, username):
+        url = f"{self.BASE_URL}/users/{username}/repos"
+        response = requests.get(url, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    def get_commits(self, username, repo_name, limit=100):
+        url = f"{self.BASE_URL}/repos/{username}/{repo_name}/commits"
+        params = {"per_page": limit}
+        response = requests.get(url, headers=self.headers, params=params)
+        response.raise_for_status()
+        return response.json()
