85ea6a32d17c6d7255ec15123aa2552f20a4b87c
Ramzi Badache <badache@estin.dz>
2026-02-06 15:11:17 +0100

    refactor main entrypoint and improve structure

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+import debugpy
 from github_api import get_user_repos
 from analyzer import analyze_all
 from html_generator import generate_portfolio
 
-username = input("GitHub username: ")
 
-repos = get_user_repos(username)
-analysis = analyze_all(username, repos)
+def main():
+    username = input("GitHub username: ").strip()
 
-generate_portfolio(username, analysis)
+    repos = get_user_repos(username)
+    analysis = analyze_all(username, repos)
 
-print(" Portfolio HTML généré : output/portfolio.html")
+    generate_portfolio(username, analysis)
+
+    print("Portfolio HTML généré : output/portfolio.html")
+
+
+if __name__ == "__main__":
+    # Active debugpy seulement si tu veux debug
+    # Lance ensuite: python main.py
+    debug_mode = False
+
+    if debug_mode:
+        debugpy.listen(("0.0.0.0", 5678))
+        print("⏳ Waiting for debugger attach on port 5678...")
+        debugpy.wait_for_client()
+        print("✅ Debugger attached!")
+
+    main()
