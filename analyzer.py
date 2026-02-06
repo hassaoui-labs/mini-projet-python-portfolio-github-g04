b9101c396972d1667bf6cb586f3f2ed08f36be3f
Ramzi Badache <badache@estin.dz>
2026-02-06 15:12:54 +0100

    make analyzer more robust with safe defaults

--- analyzer.py
+++ analyzer.py
@@ -1,34 +1,36 @@
 from github_api import get_repo_commits, get_languages
 
-def analyze_repo(username, repo):
 
-    repo_name = repo["name"]
+def analyze_repo(username, repo):
+    repo_name = repo.get("name")
 
     commits = get_repo_commits(username, repo_name)
     commit_count = len(commits) if isinstance(commits, list) else 0
 
     langs = get_languages(username, repo_name)
-    lang_list = list(langs.keys())
+    lang_list = list(langs.keys()) if isinstance(langs, dict) else []
 
-    score = commit_count*2 + repo["stargazers_count"]*5 + repo["forks_count"]*3
+    stars = repo.get("stargazers_count", 0)
+    forks = repo.get("forks_count", 0)
+
+    score = commit_count * 2 + stars * 5 + forks * 3
 
     return {
         "name": repo_name,
         "commits": commit_count,
         "languages": lang_list,
-        "stars": repo["stargazers_count"],
-        "forks": repo["forks_count"],
+        "stars": stars,
+        "forks": forks,
         "score": score,
-        "url": repo["html_url"]
+        "url": repo.get("html_url"),
     }
 
 
 def analyze_all(username, repos):
-
     results = []
 
     for repo in repos:
-        if not repo["fork"]:
+        if not repo.get("fork", False):
             results.append(analyze_repo(username, repo))
 
     return results
